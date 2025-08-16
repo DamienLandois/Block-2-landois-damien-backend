3467d77b2cf731b788b715a7a80619190844632a
DamienLandois <91963916+DamienLandois@users.noreply.github.com>
2025-08-16 21:29:22 +0200

    Ajout  feature massage + test

---
+++ prisma/migrations/20250816191649_add_position_to_massage/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE `Massage` (
+    `id` VARCHAR(191) NOT NULL,
+    `title` VARCHAR(191) NOT NULL,
+    `description` TEXT NOT NULL,
+    `image` VARCHAR(191) NOT NULL,
+    `price` DOUBLE NOT NULL,
+    `duration` INTEGER NOT NULL,
+    `position` INTEGER NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
